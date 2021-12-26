98a44252e0f98545e9dccb6eefb66ce795a00252
iheb meftah <salimbb041@gmail.com>
2021-12-26 15:29:56 +0100

    finish all section

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Profile extends StatelessWidget {
   const Profile({Key? key, String? title}) : super(key: key);
@@ -20,12 +21,16 @@ class Profile extends StatelessWidget {
                   image: const NetworkImage(
                       'https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG1vdW50YWluc3xlbnwwfHwwfHw%3D&w=1000&q=80'),
                 ),
-                const Positioned(
+                Positioned(
                     bottom: -50.0,
                     child: CircleAvatar(
                       radius: 80,
-                      backgroundImage: NetworkImage(
-                          'https://scontent.ftun7-1.fna.fbcdn.net/v/t39.30808-6/s552x414/255729438_2061962933967301_6536194202848894171_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=8WN5tCaKhNQAX8wbDdB&_nc_ht=scontent.ftun7-1.fna&oh=00_AT-vQcebr_3_dyt5y7xkSu5mO6tTdZSL7pr80y15zU8aOQ&oe=61CD0B9D'),
+                      backgroundColor: Colors.black,
+                      child: CircleAvatar(
+                        radius: 75,
+                        backgroundImage: NetworkImage(
+                            'https://scontent.ftun7-1.fna.fbcdn.net/v/t39.30808-6/s552x414/255729438_2061962933967301_6536194202848894171_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=8WN5tCaKhNQAX8wbDdB&_nc_ht=scontent.ftun7-1.fna&oh=00_AT-vQcebr_3_dyt5y7xkSu5mO6tTdZSL7pr80y15zU8aOQ&oe=61CD0B9D'),
+                      ),
                     ))
               ]),
           SizedBox(
@@ -36,10 +41,6 @@ class Profile extends StatelessWidget {
             subtitle: Center(child: Text('Computer Science Student ')),
           ),
           ListTile(
-            leading: Icon(
-              Icons.security_outlined,
-              size: 40,
-            ),
             title: Text('About me '),
             subtitle: Text(
                 'Iheb Meftah ,étudiant en Licence Science Inforamtiques specialite Anlayse De Donnees et Big Data  '),
@@ -47,11 +48,43 @@ class Profile extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          ListTile( leading: Icon(Icons.univers),
+          ListTile(
             title: Text('Education'),
             subtitle: Text(
                 'Higher Institute of Computer Science and Multimedia of Sfax '),
-          )
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ListTile(
+            title: Text('Social'),
+            subtitle: Row(
+              children: [
+                Expanded(
+                  child: IconButton(
+                      icon: FaIcon(
+                        FontAwesomeIcons.github,
+                      ),
+                      onPressed: () {}),
+                ),
+                Expanded(
+                  child: IconButton(
+                      icon: FaIcon(FontAwesomeIcons.linkedin),
+                      onPressed: () {}),
+                ),
+                Expanded(
+                  child: IconButton(
+                      icon: FaIcon(FontAwesomeIcons.facebook),
+                      onPressed: () {}),
+                ),
+                Expanded(
+                  child: IconButton(
+                      icon: FaIcon(FontAwesomeIcons.instagram),
+                      onPressed: () {}),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
